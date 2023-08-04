88baae6dbcbd4f9960c82ad54723e78aeda0704c
mital <mital@Ujatul>
2023-08-04 23:43:00 +0530

    first commit

---
+++ daythreee/src/org/tnsif/classandobjects/SandwitchE.java
@@ -0,0 +1,19 @@
+package org.tnsif.classandobjects;
+
+public class SandwitchE {
+
+	
+	
+		public static void main(String[]args) {
+			Sandwitch sc=new Sandwitch();
+			sc.breadType="Brown bread";
+			
+			sc.display();
+			
+			
+			
+		}
+
+	}
+
+
